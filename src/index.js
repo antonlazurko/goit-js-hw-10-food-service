be87123c07273175abe91c81c63d2036c49e6521
antonlazurko <lazurkoanton87@gmail.com>
2020-10-18 21:07:53 +0300

    делает дз

--- src/index.js
+++ src/index.js
@@ -8,3 +8,27 @@ const menuConteiner = document.querySelector('.js-menu')
 const menuMarkup = createMenuItem(menuItemArr);
 menuConteiner.insertAdjacentHTML("beforeend", menuMarkup)
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const checkbox = document.querySelector('#theme-switch-toggle')
+const body = document.querySelector('body')
+checkbox.addEventListener('click', onCheckboxClick)
+const savedData = localStorage.getItem('my-data');
+// console.log('savedData', savedData);
+const parsedData = JSON.parse(savedData);
+function storageChecked() { if (parsedData.checked) { checkbox.checked = parsedData.checked }}
+storageChecked()
+function storageTheme() { if (parsedData.theme) { body.classList.add(parsedData.theme) } }
+storageTheme()
+function onCheckboxClick() {
+    body.classList.toggle(Theme.DARK)
+    if (checkbox.checked) {
+        localStorage.setItem('my-data', JSON.stringify({ theme: body.classList.value, checked: true }));
+    }
+    else if (!checkbox.checked)
+    {
+        localStorage.setItem('my-data', JSON.stringify({ theme: body.classList.value,checked: false }));
+}
+    }
