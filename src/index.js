b587065c785bad276ae6b60a0650acac45997bb0
antonlazurko <lazurkoanton87@gmail.com>
2020-10-19 18:28:49 +0300

    формат Prettier, добавил отступы между логическими блоками

--- src/index.js
+++ src/index.js
@@ -1,39 +1,62 @@
-import menuItemTpl from './templates/menu-item.hbs'
-import menuItemArr from './menu.json'
-function createMenuItem(menuItemArr){
-    return menuItemTpl(menuItemArr);
+//импорт данных
+import menuItemTpl from './templates/menu-item.hbs';
+import menuItemArr from './menu.json';
 
+//----создание верстки по шаблону
+function createMenuItem(menuItemArr) {
+  return menuItemTpl(menuItemArr);
 }
-const menuConteiner = document.querySelector('.js-menu')
-const menuMarkup = createMenuItem(menuItemArr);
-menuConteiner.insertAdjacentHTML("beforeend", menuMarkup)
 
+const menuConteiner = document.querySelector('.js-menu');
+
+const menuMarkup = createMenuItem(menuItemArr);
+
+menuConteiner.insertAdjacentHTML('beforeend', menuMarkup);
+
+//----сохранение актуальной темы и положение чекбокса в Local Storage
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-const checkbox = document.querySelector('#theme-switch-toggle')
-const body = document.querySelector('body')
-checkbox.addEventListener('click', onCheckboxClick)
+
+const checkbox = document.querySelector('#theme-switch-toggle');
+
+const body = document.querySelector('body');
+
+checkbox.addEventListener('click', onCheckboxClick);
+
 const savedData = localStorage.getItem('my-data');
+
 const parsedData = JSON.parse(savedData);
+
 function storageChecked() {
-    if (parsedData)
-    {
-        checkbox.checked = parsedData.checked;
-    }
+  if (parsedData) {
+    checkbox.checked = parsedData.checked;
+  }
 }
-storageChecked()
-function storageTheme()
-{ if (parsedData) { body.classList.add(parsedData.theme) } }
-storageTheme()
+
+storageChecked();
+
+function storageTheme() {
+  if (parsedData) {
+    body.classList.add(parsedData.theme);
+  }
+}
+
+storageTheme();
+
 function onCheckboxClick() {
-    body.classList.toggle(Theme.DARK)
-    if (checkbox.checked) {
-        localStorage.setItem('my-data', JSON.stringify({ theme: body.classList.value, checked: true }));
-    }
-    else if (!checkbox.checked)
-    {
-        localStorage.setItem('my-data', JSON.stringify({ theme: body.classList.value,checked: false }));
+  body.classList.toggle(Theme.DARK);
+
+  if (checkbox.checked) {
+    localStorage.setItem(
+      'my-data',
+      JSON.stringify({ theme: body.classList.value, checked: true }),
+    );
+  } else if (!checkbox.checked) {
+    localStorage.setItem(
+      'my-data',
+      JSON.stringify({ theme: body.classList.value, checked: false }),
+    );
+  }
 }
-    }
